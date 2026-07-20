26d643c088b817679fe6dca236704edd7c594d29
aroobaraees <aroobaraees@77gmail.com>
2026-07-20 15:02:18 +0500

    chapter1

---
+++ chapter1/app.js
@@ -0,0 +1,11 @@
+// alert
+
+
+alert("Error! please enter a valid password.");
+
+alert("welcome to js land...");
+
+alert("happy coding!");
+
+
+alert("Hello... i can run js through web browser console");
