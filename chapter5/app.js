7165a6217998f293bda59371acceb3e7dbbf940c
aroobaraees <aroobaraees@77gmail.com>
2026-08-02 21:37:31 +0500

    9-11 chapter is done

--- chapter5/app.js
+++ chapter5/app.js
@@ -23,37 +23,197 @@
 // Q3
 
 // a
-var num;
+// var num;
 
 // b
-document.write("Value after variable declaration is: " + num + "<br>");
+// document.write("Value after variable declaration is: " + num + "<br>");
 
 // c
-num = 5;
+// num = 5;
 
 // d
-document.write("Initial value: " + num + "<br>");
+// document.write("Initial value: " + num + "<br>");
 
 // e
-num++;
+// num++;
 
 // f
-document.write("Value after increment is: " + num + "<br>");
+// document.write("Value after increment is: " + num + "<br>");
 
 // g
-num = num + 7;
+// num = num + 7;
 
 // h
-document.write("Value after addition is: " + num + "<br>");
+// document.write("Value after addition is: " + num + "<br>");
 
 // i
-num--;
+// num--;
 
 // j
-document.write("Value after decrement is: " + num + "<br>");
+// document.write("Value after decrement is: " + num + "<br>");
 
 // k
-var remainder = num % 3;
+// var remainder = num % 3;
 
 // l
-document.write("The remainder is: " + remainder);
+// document.write("The remainder is: " + remainder);
+
+
+
+// Q 4
+
+// var ticketPrice = 600;
+// var totalCost = ticketPrice * 5;
+
+// document.write("Total cost to buy 5 ticket to a movie is " +  totalCost + " PKR");
+
+// Q 5
+
+// var num = 4;
+// document.write("Table of " + num + "<br>");
+// document.write(num + " x 1 = " + num * 1 + "<br>");
+// document.write(num + " x 2 = " + num * 2 + "<br>");
+// document.write(num + " x 3 = " + num * 3 + "<br>");
+// document.write(num + " x 4 = " + num * 4 + "<br>");
+// document.write(num + " x 5 = " + num * 5 + "<br>");
+// document.write(num + " x 6 = " + num * 6 + "<br>");
+// document.write(num + " x 7 = " + num * 7 + "<br>");
+// document.write(num + " x 8 = " + num * 8 + "<br>");
+// document.write(num + " x 9 = " + num * 9 + "<br>");
+// document.write(num + " x 10 = " + num *10 + "<br>");
+
+// Q 6
+
+
+// var celsius = 25;
+
+// var fahrenheit = (celsius * 9 / 5) + 32;
+
+// document.write(celsius + " C is " + fahrenheit + "F<br>");
+
+// var fahrenheitTemp = 70;
+
+// var celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+// document.write(fahrenheitTemp + "F is " + celsiusTemp + "C");
+
+// Q 7
+
+// var price1 = 650;
+// var price2 = 100;
+
+// var quntity1 = 3;
+// var quntity2 = 7;
+
+// var shopping = 100;
+
+// var totalCost = (price1 * quntity1) + (price2 * quntity2) + shopping;
+
+// document.write("Total cost of your order is " + totalCost + " PKR")
+
+
+// Q 8
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("Marks Sheet");
+// document.write("<br><br>");
+
+// document.write("Total marks: " + totalMarks);
+// document.write("<br>");
+
+// document.write("Marks obtained: " + marksObtained);
+// document.write("<br>");
+
+// document.write("Percentage: " + percentage + "%");
+
+// Q9
+
+// var dollars = 10;
+// var riyals = 25;
+
+// var dollarRate = 104.80;
+// var riyalRate = 28;
+
+// var dollarInPKR = dollars * dollarRate;
+// var riyalInPKR = riyals * riyalRate;
+
+// var totalPKR = dollarInPKR + riyalInPKR;
+
+// document.write("Currency in PKR");
+// document.write("<br><br>");
+
+// document.write("Total Currency in PKR: " + totalPKR);
+
+// Q10
+
+// Q10
+// var num = 5;
+
+// var result = ((num + 5) * 10) / 2;
+
+// document.write("Result: " + result);
+
+// Q11
+
+// Q11
+// var currentYear = 2026;
+// var birthYear = 2000;
+
+// var age1 = currentYear - birthYear;
+// var age2 = age1 - 1;
+
+// document.write("Age Calculator");
+// document.write("<br><br>");
+
+// document.write("Current Year: " + currentYear);
+// document.write("<br>");
+
+// document.write("Birth Year: " + birthYear);
+// document.write("<br>");
+
+// document.write("They are either " + age2 + " or " + age1 + " years old.");
+
+
+// Q12
+// var radius = 20;
+// var pi = 3.142;
+
+// var circumference = 2 * pi * radius;
+// var area = pi * radius * radius;
+
+// document.write("The Geometrizer");
+// document.write("<br><br>");
+
+// document.write("Radius of a circle: " + radius);
+// document.write("<br>");
+
+// document.write("The circumference is: " + circumference);
+// document.write("<br>");
+
+// document.write("The area is: " + area);
+
+
+// Q13
+// var currentAge = 20;
+// var maxAge = 80;
+// var amountPerDay = 2;
+
+// var totalNeeded = (amountPerDay * 365) * (maxAge - currentAge);
+
+// document.write("Lifetime Supply Calculator");
+// document.write("<br><br>");
+
+// document.write("Current Age: " + currentAge);
+// document.write("<br>");
+
+// document.write("Maximum Age: " + maxAge);
+// document.write("<br>");
+
+// document.write("Amount Per Day: " + amountPerDay);
+// document.write("<br>");
+
+// document.write("You will need " + totalNeeded + " snacks to last you until the ripe old age of " + maxAge);
---
+++ chapter9-11/app.js
@@ -0,0 +1,150 @@
+// Q1
+
+// var city = prompt("Enter your city name");
+
+// if (city === "karachi") {
+//     alert("welcome to city of lights");
+
+// }
+// else{
+//     alert("buy buy");
+// }
+
+// Q2
+
+
+// var gender = prompt("Enter your gender");
+
+// if (gender === "male") {
+//     alert("good morning sir");
+
+// }
+
+// else if (gender === "female") {
+//  alert("good morning mam");
+
+// }
+
+// Q3
+
+// var color = prompt("Enter your fraffic signal color");
+
+// if (color === "red"){
+//     alert("mous stop");
+// }
+
+// else if (color === "yellow"){
+//     alert("Ready to move");
+// }
+
+// else if (color === "green"){
+//     alert("move now");
+// }
+
+// Q4
+
+// var fuel = prompt("Entewr remaining fuel in litres:");
+
+// if (fuel <0.25) {
+//     alert("please refill the fuel in your car");
+// }
+
+
+// Q5
+
+
+
+// Q6
+
+// Q7
+
+// var secretNumber = 5;
+
+// var huess = +prompt("guess the secret number 1 to 10:");
+
+// if (guess === secretNumber ) {
+//     alert("Bingo ! correct answer");
+// }
+
+// else if (guess === secretNumber + 1) {
+//     alert("close enough of the correct answer");
+// }
+
+
+// Q8
+
+// var number = +prompt("Enter a number:");
+
+// if (number % 3 === 0) {
+//     alert("The number is divisible by 3");
+// }
+
+// Q9
+
+
+// var number = +prompt("Enter a number:");
+
+// if (number % 2 === 0){
+//     alert("the number is even");
+// }
+
+// else {
+//     alert("the number is odd");
+// }
+
+
+// Q10
+
+// var temperature = +prompt("Enter temperature:");
+
+// if (temperature > 40){
+//     alert("it is too hot outside");
+// }
+
+// else if (temperature > 30) {
+//     alert("The weather today is normal");
+// }
+
+
+// else if (temperature > 20) {
+//     alert("Today weather is cool:");
+// }
+
+
+// else if (temperature > 10) {
+//     alert("OMG! Today is weather is so cool");
+// }
+
+// else {
+//     alert("temperature is 10 or below");
+// }
+
+
+// 11 q
+
+var num1 = +prompt("Enter first number:");
+var num2 = +prompt("Enter second number:");
+var operation = prompt("Enter operation (+, -, *, /, %):");
+
+var result;
+
+if (operation === "+") {
+    result = num1 + num2;
+}
+else if (operation === "-") {
+    result = num1 - num2;
+}
+else if (operation === "*") {
+    result = num1 * num2;
+}
+else if (operation === "/") {
+    result = num1 / num2;
+}
+else if (operation === "%") {
+    result = num1 % num2;
+}
+else {
+    result = "Invalid operation";
+}
+
+alert("Result = " + result);
