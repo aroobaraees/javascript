a3ce7255b15039325a5a06ab805d6ba648e95f4c
aroobaraees <aroobaraees@77gmail.com>
2026-08-12 22:45:32 +0500

    chapter 14 done

---
+++ chapter14-16/app.js
@@ -0,0 +1,87 @@
+
+
+
+
+// chapter arrays
+
+
+
+
+// Q1      empty array using js literal notation
+
+// var studentName = []
+
+// console.log("[]");
+
+  
+
+
+// Q2       empty array using js object notation
+
+
+// var studentNames = new Array();
+// console.log(studentNames);
+
+
+// Q3    strings array
+
+
+
+// var teaName = ["Doodh Patti Chai", "Karak Chai", "Green Tea", "Kashmiri Chai", "Masala Chai",];
+// console.log(teaName);
+
+
+// Q4 numbers array
+
+
+// var numbers = ["10", "20", "30", "40", "50", "60", "70", "80", "90","100"]
+// console.log(numbers);
+
+
+// Q5   Boolean array
+
+
+// var status = [true, false, true, false];
+// console.log(status);
+
+
+// Q6     mixed arrays
+
+// var mixedArrays = ["Arooba", 19, "Karachi", true,];
+// console.log(mixedArrays);
+
+
+
+// Q7 qualifications array
+
+// var qualifications = [
+//     "SSC",
+//     "HSC",
+//     "BCS",
+//     "BS",
+//     "BCOM",
+//     "MS",
+//    " M.phil",
+//    "PHD",
+// ];
+// document.write("<h2>qualifications;</h2>");
+
+// for(var i = 0; i <qualifications.length; i++) {
+//     document.write((i + 1) + ") " + qualifications[i] + "<br>");
+// }
+
+
+// Q8     student names and percentages
+
+
+
+// var studentsName = ["Micheal ", "Jhon ", "Tony "];
+
+// var scores = [320, 230, 480];
+
+// var totalMarks = 500;
+
+// for (var i = 0; i <studentsName.length; i++) {
+//     var percentage = ( scores [i] / totalMarks ) * 100;
+//     document.write(" scores of " +  studentsName [i] + " is " + scores [i] + " percentage " + percentage +  "% <br>");
+// }
--- practice/app.js
+++ practice/app.js
@@ -5,6 +5,6 @@
 
 
 let num = ["arooba", "harmain", "rimsha", "emaan", "ravish", "yusra"]
-num.slice()
+num.slice(emaan)
 
 console.log(num)
