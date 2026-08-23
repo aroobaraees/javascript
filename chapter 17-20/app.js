b66f8c1213d52a2666fc6af223552d54457c8e44
aroobaraees <aroobaraees@77gmail.com>
2026-08-24 02:30:36 +0500

    chqpter 17 to 20 done

---
+++ chapter 17-20/app.js
@@ -0,0 +1,85 @@
+
+// Q2
+
+
+
+// let arr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//     document.write(arr[i].join(" ") + "<br>");
+// }
+
+
+
+// Q3
+
+
+// for (let i = 1; i <= 10; i++) {
+//     document.write(i + "<br>");
+// }
+
+
+// Q4
+// 
+
+// let num = +prompt("Enter a number to show its multiplication table");
+
+// document.write("Multiplication table of " + num + "<br>");
+// document.write("Length 15<br><br>");
+
+// for (let i = 1; i <= 15; i++) {
+//     document.write(num + " x " + i + " = " + (num * i) + "<br>");
+// }
+
+
+
+// Q5
+
+
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "<br>");
+// }
+
+// document.write("<br>");
+
+// for (let i = 0; i < fruits.length; i++) {
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br>");
+// }
+
+
+
+// Q 6
+
+
+
+// for (let i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+
+
+// for (let i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+
+
+// for (let i = 2; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+
+
+// for (let i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+
+
+
+// for (let i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+ 
